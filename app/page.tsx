fa74cd2267caae080ad2af7b13f8f225cd7b3e3f
thatjuancodes <thejuan.codes@gmail.com>
2024-09-25 14:04:51 +0700

    add todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 export default function Home() {
+  // TODOS:
+  // 1. Do a fetch from OPEN AI api
+  // 2. Display the response
+  // 3. Use streaming strategy to display the response
+  // 4. Add a form to take user input
+  // 5. Improve the UI/UX
+  // 6. Update README
+
   return (
     <div>
       <h1 className="mt-4 text-4xl font-bold text-center">Open AI Prompter - Built on Next JS</h1>
