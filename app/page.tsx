f34e268c5e7fec1347562f97e2ec678a3e43f1ea
thatjuancodes <thejuan.codes@gmail.com>
2024-09-26 15:19:38 +0700

    add scroll to bottom function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import ReactMarkdown from 'react-markdown'
 import TextInput from "./components/TextInput"
 
@@ -15,6 +15,7 @@ export default function Home() {
   const [openAIResponse, setOpenAIResponse] = useState<string | null>('')
   const [userInput, setUserInput] = useState('')
   const [isSubmitted, setIsSubmitted] = useState(false)
+  const responseEndRef = useRef<HTMLDivElement | null>(null)
 
   const handleOpenAIRequest = async () => {
     setIsSubmitted(true)
@@ -45,6 +46,10 @@ export default function Home() {
         done = doneReading
         const chunk = decoder.decode(value, { stream: true })
         setOpenAIResponse((prev) => prev + chunk)
+      
+        // move to the bottom of the scroll view while we the response streams in
+        if (responseEndRef.current === null) return
+        responseEndRef.current.scrollIntoView({ behavior: 'smooth' })
       }
     }
     catch (error) {
@@ -53,21 +58,25 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center p-20">
-      <h1 className="mt-4 text-4xl font-bold text-center">Open AI Prompter - Built on Next JS</h1>
+    <div className="flex flex-col items-center p-20 h-screen">
+      <h1 className="text-4xl font-bold text-center">Open AI Prompter - Built on Next JS</h1>
 
-      <div className="text-left mt-10">
-        {openAIResponse &&
-          // additional render functions need to be used to make the headings work
-          <ReactMarkdown>{openAIResponse}</ReactMarkdown>
-        }
-      </div>
+      {isSubmitted && (
+        <div className="text-left text-lg text-gray-900 mt-10 h-2/3 pb-5 w-full overflow-y-scroll">
+          {openAIResponse &&
+            // additional render functions need to be used to make the headings work
+            <ReactMarkdown>{openAIResponse}</ReactMarkdown>
+          }
+
+          <div ref={responseEndRef} />
+        </div>
+      )}
 
       <div className={`${isSubmitted ? 'fixed bottom-0 left-16 right-16 pb-10' : 'relative w-full'}`}>
         <TextInput label="Enter your prompt..." value={userInput} onChange={setUserInput} />
 
         <button
-          className="mt-5 w-full px-5 py-3 bg-green-500 text-white"
+          className="mt-5 w-full px-5 py-3 bg-blue-700 text-white text-2xl hover:bg-blue-600"
           onClick={handleOpenAIRequest}
         >
           Submit
